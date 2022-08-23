da5f5da640accd3643cdb617df07830bba394d79
Sbonga Nzimande <Nzimandesbonga@gmail.com>
2022-08-23 20:43:54 +0200

    second commit

--- pizza-cart-api.js
+++ pizza-cart-api.js
@@ -14,6 +14,9 @@ document.addEventListener('alpine:init', () => {
            .then((result) => {
              // console.log(result.data);
              this.pizzas = result.data.pizzas
+             this.smallpizzas = this.pizzas[11];
+             this.mediumFeatured = this.pizzas[7];
+             this.largeFeatured = this.pizzas[25];
             })
             .then(() => {
              return this.createCart();
@@ -26,6 +29,22 @@ document.addEventListener('alpine:init', () => {
             });
          
       },
+
+    buys:true,
+    payCart(amount){
+    const body = {
+    cart_code: this.cartID
+  }
+  amount = this.payment
+  axios.post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/pay', body)
+  .then((result)=>{
+    // this.payment=result.data.payment
+    // this.cartID = result.data.cart_code
+    this.message = 'paym'
+    this.showCart()
+  });
+},
+
       createCart() {
           // /api/pizza-cart/create
           return axios.get('https://pizza-cart-api.herokuapp.com/api/pizza-cart/create?username=' + this.username) 
@@ -45,12 +64,55 @@ document.addEventListener('alpine:init', () => {
       pizzaImage(pizza) {
         return `/img/${pizza.size}.jpg`
       },
+
+      checkb:true,
+      payNow:false,
+      
+      
+      makePayment(){
+        if(!this.paymentAmount){
+          this.returnFeedback = 'please enter amount'
+      }
+      else if ((this.cart.total) > this.paymentAmount){
+          this.returnFeedback = 'Sorry - that is not enough money!'
+      }
+      else{
+          this.returnFeedback = 'Enjoy your pizzas'
+      
+        
+      
+      setTimeout(()=>{
+        if((this.cart.total) <= this.paymentAmount){
+          this.appreciation = "Thank you, refresh page to create a new order"
+          }
+          
+          this.payNow=false;
+          this.clearCart()
+          this.pizzas=true
+          this.appreciation
+          this.cartId=''
+          this.clearAtPay=false
+          this.titleHide=false
+      }, 30000)
+  }
+},
+
+paymentAmount : 0,
+
+  clearCart(){
+    this.cart=0.0
+
+  },
        
        message : 'Eating pizzas',
        username : 'nzimande',
        pizzas : [],
        cartId : '',
        cart : { total : 0 },
+       returnFeedback : '',
+       smallpizzas:[],
+       mediumFeatured:[],
+       largeFeatured:[],
 
        add(pizza) {
         // to be able to add a pizza to the cart I need cart Id...
@@ -72,8 +134,33 @@ document.addEventListener('alpine:init', () => {
 
        // alert(pizza.id)
 
+       },
+
+       subtract(pizza) {
+        // to be able to add a pizza to the cart I need cart Id...
+        
+        const params = {
+          cart_code: this.cartId,
+          pizza_id: pizza.id
+        }
+        
+        axios
+            .post('https://pizza-cart-api.herokuapp.com/api/pizza-cart/remove', params)
+            .then(() => {
+              
+              this.message = "Pizza removed from the cart" 
+              this.showCart();
+            })
+            .catch(err => alert(err) );
+
+
+       // alert(pizza.id)
+
        }
     
     }
     });
 })
+
+
+  
